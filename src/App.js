8da4f21f140b66a0cf3299cda7b1ad5efff0e2b8
mujtahid3996 <mujtahid2018@gmail.com>
2019-02-12 11:33:41 +0600

    my frist working with react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React from 'react';
+import Cardlist from './Cardlist';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+const App = () =>{
+	return(
+		<div className='tc'>
+			<h1>RoboFriends</h1>
+			<SearchBox />
+			<Cardlist robots = {robots}/>
+		</div>
+		);
 }
-
-export default App;
+export default App; 
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Card =({ name,email,id }) =>
+{
+	return (
+		<div className="tc bg-light-green dib pa3 ma2 grow bw2 shadow5">
+			<img alt='robots' src={`https://robohash.org/${id}`}/>
+			<div >
+				<h2> {name}</h2>
+				<p> {email}</p>  
+			</div>
+		</div>
+		);
+}
+export default Card;
